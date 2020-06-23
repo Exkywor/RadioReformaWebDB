d29a0970bea5eaffbce0d2a051de86d2b18b3077
Exkywor <fernandoquant.rs@gmail.com>
2020-06-23 16:40:06 -0600

    Added functionality to change timezone from dropdown list in webpage and display the updated programs/scheudle

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify, render_template, request, redirect
 import datetime, pytz, sqlite3
+from flask import Flask, jsonify, render_template, request, redirect, session
+from flask_session import Session
+from tempfile import mkdtemp
 
 from utils.prepareDB import prepareDB
 
@@ -15,49 +17,64 @@ def after_request(response):
   response.headers["Pragma"] = "no-cache"
   return response
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 # Open the db
 db = sqlite3.connect("programs.db", check_same_thread=False)
 db.row_factory = sqlite3.Row # We'll use this to convert the tuples into rows
 cursor = db.cursor()
-# This would give us tuples, so we changed it to get Rows. We'll convert them into a dict later
-cursor.execute("SELECT * from Programs")
-programsToDict = cursor.fetchall()
-cursor.execute("SELECT * from Airs") # We could join programs and airs but it would make it harder to filter an iterate through empty days
-airingToDict = cursor.fetchall()
 
-# We extract the information from the Rows and convert them to dicts
-programsList = []
-for program in programsToDict:
-  programsList.append({key:program[key] for key in program.keys()})
-airingList = []
-for airing in airingToDict:
-  airingList.append({key:airing[key] for key in airing.keys()})
-
-# Timezone variables
-timezone = "America/El_Salvador"
-timeoffset = -360
-
-# Prepares the programs and schedule
-processed = prepareDB(programsList, airingList, timeoffset)
-programs = processed["programs"]
-schedule = processed["schedule"]
+# Sets the programs and the schedule, which we'll use in the app
+def getData():
+  processed = prepareDB(session["programsList"], session["airingList"], session["timeoffset"])
+  session["programs"] = processed["programs"]
+  session["schedule"] = processed["schedule"]
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+  # This would give us tuples, so we changed it to get Rows. We'll convert them into a dict later
+  cursor.execute("SELECT * from Programs")
+  programsToDict = cursor.fetchall()
+  cursor.execute("SELECT * from Airs") # We could join programs and airs but it would make it harder to filter an iterate through empty days
+  airingToDict = cursor.fetchall()
+
+  # We extract the information from the Rows and convert them to dicts
+  programsList = []
+  for program in programsToDict:
+    programsList.append({key:program[key] for key in program.keys()})
+  airingList = []
+  for airing in airingToDict:
+    airingList.append({key:airing[key] for key in airing.keys()})
+
+  session["programsList"] = programsList
+  session["airingList"] = airingList
+
+  # Timezone variables
+  if session.get("timezone") is None:
+    session["timezone"] = "America/El_Salvador"
+  if session.get("timeoffset") is None:
+    session["timeoffset"] = -360
+
+  getData()
+
   return render_template("index.html", title="Programas")
 
 
 @app.route("/programs", methods=["GET", "POST"])
 def getPrograms():
   if request.method == "POST":
-    return jsonify(programs)
+    return jsonify(session["programs"])
 
 
 @app.route("/schedule", methods=["GET", "POST"])
 def getSchedule():
   if request.method == "POST":
-    return jsonify(schedule)
+    return jsonify(session["schedule"])
 
   return render_template("schedule.html", title="Horarios")
 
@@ -65,23 +82,16 @@ def getSchedule():
 @app.route("/getTimezone", methods=["POST"])
 def getTimezone():
   if request.method == "POST":
-    return jsonify(timezone=timezone, timezones=pytz.common_timezones)
+    return jsonify(timezone=session["timezone"], timezones=pytz.common_timezones)
 
 
 @app.route("/changeTimezone", methods=["POST"])
 def changeTimezone():
   if request.method == "POST":
-    timezone = request.form.get("timezone")
+    session["timezone"] = request.form.get("timezone")
     # We get the utcoffset in seconds, then convert it to minutes, then use int() to remove the decimal point
-    timeoffset = int(datetime.datetime.now(pytz.timezone(timezone)).utcoffset().total_seconds()/60)
+    session["timeoffset"] = int(datetime.datetime.now(pytz.timezone(session["timezone"])).utcoffset().total_seconds()/60)
 
-    # -360, 0, -420, 720
-    prepareDB(programsList, airingList, timeoffset)
+    getData()
 
-    return jsonify(timezone=timezone)
-    
-# ROUTE TO HANDLE TIMECHANGE
-  # Change the timezone
-  # Call preparedDB again (add the timezone as a parameter)
-  # Change programs and schedule variables
-  # Return true
+    return jsonify(timezone=session["timezone"])
