683729f08c7ac9820d60eb206938d6d51ac97253
Exkywor <fernandoquant.rs@gmail.com>
2020-07-01 00:51:27 -0600

    Finished functionality to add programs to the DB, they get properly added!

--- utils/modifyDB.py
+++ utils/modifyDB.py
@@ -10,7 +10,7 @@ def editProgram(changes, db, offset, indexedSchedule, programs):
   # Store what we're going to do to the DB
   infoToChange = {}
   scheduleToChange = {}
-  
+
   if bool(schedule): # Check that there are changes in the schedule
     # Validate schedule
     scheduleValidated = validateSchedule(schedule, indexedSchedule, id)
@@ -23,7 +23,7 @@ def editProgram(changes, db, offset, indexedSchedule, programs):
       if isEmpty(schedule[day]): # If empty we'll set it to None (NULL)
         scheduleToChange[day] = None
       else:
-        toTimezoneSchedule[day] = schedule[day].strip().split(", ") 
+        toTimezoneSchedule[day] = schedule[day].strip().split(", ")
 
     # Covert the schedule to UTC time (the opposite of the current offset)
     timezoned = changeTimezone(toTimezoneSchedule, offset*-1)
@@ -37,7 +37,7 @@ def editProgram(changes, db, offset, indexedSchedule, programs):
     infoValidated = validateInfo(info, programs, id)
     if infoValidated["res"] is False:
       return infoValidated["message"]
-    
+
     # Prepare the elements to change
     for key in info:
       if key == "length":
@@ -77,7 +77,7 @@ def addProgram(changes, db, offset, indexedSchedule, programs):
   # Store what we're going to do to the DB
   infoToChange = {}
   scheduleToChange = {}
-  
+
   # Validate schedule
   scheduleValidated = validateSchedule(schedule, indexedSchedule, id, "add")
   if scheduleValidated["res"] is False:
@@ -89,7 +89,7 @@ def addProgram(changes, db, offset, indexedSchedule, programs):
     if isEmpty(schedule[day]): # If empty we'll set it to None (NULL)
       scheduleToChange[day] = None
     else:
-      toTimezoneSchedule[day] = schedule[day].strip().split(", ") 
+      toTimezoneSchedule[day] = schedule[day].strip().split(", ")
 
   # Covert the schedule to UTC time (the opposite of the current offset)
   timezoned = changeTimezone(toTimezoneSchedule, offset*-1)
@@ -102,7 +102,7 @@ def addProgram(changes, db, offset, indexedSchedule, programs):
   infoValidated = validateInfo(info, programs, id, "add")
   if infoValidated["res"] is False:
     return infoValidated["message"]
-  
+
   # Prepare the elements to change
   for key in info:
     if key == "length":
@@ -121,16 +121,54 @@ def addProgram(changes, db, offset, indexedSchedule, programs):
     else:
       infoToChange[key] = info[key].strip()
 
-  # # EDIT THE DB
-  # cursor = db.cursor()
-  # # Update information
-  # if bool(infoToChange):
-  #   for item in infoToChange:
-  #     cursor.execute(f"UPDATE Programs SET {item}=? WHERE programID={id}", [infoToChange[item]])
-  #   db.commit()
-  # if bool(scheduleToChange):
-  #   for item in scheduleToChange:
-  #     cursor.execute(f"UPDATE Airs SET {item}=? WHERE programID={id}", [scheduleToChange[item]])
-  #   db.commit()
+  # Set any non-required value to None
+  # We use this in order to be able to write a static SQL execute string without worrying that a key does not exist in the changes
+  if "presenters" not in infoToChange:
+    infoToChange["presenters"] = None
+  if "image" not in infoToChange:
+    infoToChange["image"] = f"library/assets/programsImages/{infoToChange['name'].lower().replace(' ', '-')}"
+  scheduleNonRequired = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+  for day in scheduleNonRequired:
+    if day not in scheduleToChange:
+      scheduleToChange[day] = None
+
+  # ADD TO THE DB
+  cursor = db.cursor()
+
+  # Update Programs
+  infoValues = [
+    infoToChange["name"],
+    infoToChange["streamID"],
+    infoToChange["length"],
+    infoToChange["image"],
+    infoToChange["author"],
+    infoToChange["presenters"],
+    infoToChange["topics"],
+    infoToChange["descriptionShort"],
+    infoToChange["descriptionLong"]
+  ]
+  info = ''' INSERT INTO Programs(name, streamID, length, image, author, presenters, topics, descriptionShort, descriptionLong)
+            VALUES(?,?,?,?,?,?,?,?,?)
+        '''
+  cursor.execute(info, infoValues)
+  programID = cursor.lastrowid
+
+  # Update Schedule
+  scheduleValues = [
+    programID,
+    scheduleToChange["monday"],
+    scheduleToChange["tuesday"],
+    scheduleToChange["wednesday"],
+    scheduleToChange["thursday"],
+    scheduleToChange["friday"],
+    scheduleToChange["saturday"],
+    scheduleToChange["sunday"]
+  ]
+  schedule = ''' INSERT INTO Airs(programID, monday, tuesday, wednesday, thursday, friday, saturday, sunday)
+                VALUES(?,?,?,?,?,?,?,?)
+            '''
+  cursor.execute(schedule, scheduleValues)
+
+  db.commit()
 
   return "Éxito"
